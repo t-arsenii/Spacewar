d7cda0aebe1f420e336b302f3d7d244dbea2a91b
Arsenii Tsarenko <senyazp80@gmail.com>
2025-06-29 15:03:21 +0200

    Improve equipement model

--- Assets/Features/Equipment/EquipementModel.cs
+++ Assets/Features/Equipment/EquipementModel.cs
@@ -8,13 +8,34 @@ public enum WeaponType
      Railgun,
      RocketLauncher
 }
+
+public enum SelectedWeapon
+{
+     DefaultWeapon,
+     AdditionalWeapon
+}
 public class EquipementModel
 {
-     private WeaponType DefaultWeapon = WeaponType.Rifle;
-     private WeaponType? AdditionalWeapon;
+     private readonly WeaponType DefaultWeapon = WeaponType.Rifle;
+     public WeaponType? AdditionalWeapon { get; private set; } = null;
+     public SelectedWeapon SelectedWeapon { get; private set; } = SelectedWeapon.DefaultWeapon;
 
      public void SetAdditionalWeapon(WeaponType weaponType)
      {
           AdditionalWeapon = weaponType;
+     }
+     public void SetSelectedWeapon(SelectedWeapon selectedWeapon)
+     {
+
+          if (selectedWeapon == SelectedWeapon.DefaultWeapon)
+          {
+               SelectedWeapon = selectedWeapon;
+               return;
+          }
+
+          if (AdditionalWeapon is not null)
+          {
+               SelectedWeapon = selectedWeapon;
+          }
      }
 }
--- Assets/Features/Equipment/EquipmentController.cs
+++ Assets/Features/Equipment/EquipmentController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EquipmentController : MonoBehaviour, IEquipmentController
 {
      private EquipementModel equipement;
+
+     public SelectedWeapon SelectedWeapon => equipement.SelectedWeapon;
+
+     public WeaponType? AdditionalWeapon => equipement.AdditionalWeapon;
+
      void Awake()
      {
           equipement = new EquipementModel();
@@ -14,9 +19,27 @@ public class EquipmentController : MonoBehaviour, IEquipmentController
      {
           equipement.SetAdditionalWeapon(item);
      }
+
+     public void SelectWeapon(SelectedWeapon selectedWeapon)
+     {
+          if (selectedWeapon != SelectedWeapon.AdditionalWeapon)
+          {
+               equipement.SetSelectedWeapon(selectedWeapon);
+               return;
+          }
+
+          if (AdditionalWeapon is null)
+          {
+               return;
+          }
+          equipement.SetSelectedWeapon(selectedWeapon);
+     }
 }
 
 interface IEquipmentController
 {
      public void PickupWeapon(WeaponType item);
+     public void SelectWeapon(SelectedWeapon selectedWeapon);
+     public SelectedWeapon SelectedWeapon { get; }
+     public WeaponType? AdditionalWeapon { get; }
 }
